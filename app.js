ab7b4cfe1cc6e11103cf66f024ad4bc39dfddb73
dylanwalls <dylan.walls@bitprop.com>
2023-08-31 08:22:11 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files (e.g., your HTML file)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
