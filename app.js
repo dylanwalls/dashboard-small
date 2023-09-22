08d4b1c86ff9cf986161cc42e1f8e9fe239c3816
dylanwalls <dylan.walls@bitprop.com>
2023-09-22 15:40:57 +0200

    add password protection

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const path = require('path');
+const basicAuth = require('basic-auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const users = {
+  'dylan.walls@bitprop.com': 'bitprop2023',
+  'brittany.newton@bitprop.com': 'bitprop2023',
+  'buhle.gqola@bitprop.com': 'bitprop2023',
+  'jon.fisher@bitprop.com': 'bitprop2023',
+};
+
+// Middleware for basic authentication
+const basicAuthMiddleware = (req, res, next) => {
+  const user = basicAuth(req);
+
+  if (!user || !users[user.name] || users[user.name] !== user.pass) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.status(401).send('Authentication required.');
+  }
+
+  // Authentication successful, continue to the next middleware or route
+  next();
+};
+
+app.use(basicAuthMiddleware);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
